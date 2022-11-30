5fc70d5d5d8f45c1e5fd7656f7b294a2194b1dfe
radek.los <radek.los@ataccama.com>
2022-11-30 22:58:22 +0100

    Run test on MySQL

--- cloudlock-spring/build.gradle.kts
+++ cloudlock-spring/build.gradle.kts
@@ -11,12 +11,16 @@ dependencies {
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:2.3.2")
     implementation("javax.xml.bind:jaxb-api:2.3.1")
 
+
+
+
     implementation(libs.spring.boot.starter.data.jdbc)
     implementation(libs.spring.boot.starter.web)
 
     implementation(libs.kotlin.logging.jvm)
     testImplementation(libs.h2)
     testImplementation(libs.postgresql)
+    testImplementation("mysql:mysql-connector-java:8.0.31")
     testImplementation(libs.bundles.kotest)
     testImplementation(libs.bundles.spring.test)
 }
